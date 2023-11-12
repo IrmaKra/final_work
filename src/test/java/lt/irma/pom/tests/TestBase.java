c6b09d7c03057327971341b55f5ac9649268474a
Irma <irma.krajauskiene@gmail.com>
2023-11-12 15:04:42 +0200

    Add Test Listener

--- src/test/java/lt/irma/pom/tests/TestBase.java
+++ src/test/java/lt/irma/pom/tests/TestBase.java
@@ -1,9 +1,12 @@
 package lt.irma.pom.tests;
 
 import lt.irma.pom.pages.Common;
+import lt.irma.pom.utilities.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class TestBase {
     @BeforeMethod
     public abstract void setUp();
